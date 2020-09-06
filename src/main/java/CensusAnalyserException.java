cfb70a01b273d9554afb2b984a10ccf8be318cc7
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-09-06 21:22:14 +0530

    REFACTORED:To handle custom Exception

--- src/main/java/CensusAnalyserException.java
+++ src/main/java/CensusAnalyserException.java
@@ -4,7 +4,12 @@ public class CensusAnalyserException extends Exception {
         CENSUS_FILE_PROBLEM,
         INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER
     }
+
     ExceptionType type;
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type =type;
+    }
 
     public CensusAnalyserException(String message,ExceptionType type,Throwable cause)
     {
