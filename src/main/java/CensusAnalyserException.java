dd599eade1d0413d9aea4b21006c86c17c1323af
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-09-07 14:19:03 +0530

    REFACTORED:IllegalStateException type added

--- src/main/java/CensusAnalyserException.java
+++ src/main/java/CensusAnalyserException.java
@@ -1,19 +1,14 @@
 public class CensusAnalyserException extends Exception {
-    enum ExceptionType
+    public enum ExceptionType
     {
         CENSUS_FILE_PROBLEM,
-        INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER
+        INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER,
+        UNABLE_TO_PARSE
     }
 
     ExceptionType type;
     public CensusAnalyserException(String message, ExceptionType type) {
         super(message);
         this.type =type;
-    }
-
-    public CensusAnalyserException(String message,ExceptionType type,Throwable cause)
-    {
-        super(message,cause);
-        this.type = type;
     }
 }
